5978a66006d883a848031a594240f867919ab63a
Łukasz Klekner <lukaszklekner@gmail.com>
2021-10-24 11:06:28 +0200

    implements Button component

---
+++ src/components/Button.js
@@ -0,0 +1,19 @@
+import PropTypes from 'prop-types'
+
+const Button = ({ text, color, onClick }) => (
+  <button onClick={onClick} className='btn' style={{ backgroundColor: color }}>
+    {text}
+  </button>
+)
+
+Button.defaultProps = {
+  color: 'steelblue',
+}
+
+Button.propTypes = {
+  text: PropTypes.string,
+  color: PropTypes.string,
+  onClick: PropTypes.func,
+}
+
+export default Button
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,18 @@
 import PropTypes from 'prop-types'
+import Button from './Button'
 
-const Header = ({ title }) => (
-  <header>
-    <h1>{title}</h1>
-  </header>
-)
+const Header = ({ title }) => {
+  const onClick = () => {
+    console.log('click')
+  }
+
+  return (
+    <header className='header'>
+      <h1>{title}</h1>
+      <Button text='Add' color='green' onClick={onClick} />
+    </header>
+  )
+}
 
 Header.defaultProps = {
   title: 'Hello Lukasz',
